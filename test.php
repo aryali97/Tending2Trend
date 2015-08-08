be798c4e1aa437009d13cb20367e4be5f5b9c9c2
aryali97 <aryali97@gmail.com>
2015-08-08 15:49:44 -0400

    test3

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-echo "penis";
 require_once('TwitterAPIExchange.php');
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
@@ -9,4 +8,11 @@ $settings = array(
     'consumer_key' => "QTe03opZ9XcXsLwj55BRXb4qx",
     'consumer_secret' => "wRWOKG7ofG4604cgg0uTVOSdmWuVu4lL8AMCEdq3XNI1TuUn3U"
 );
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$requestMethod = "GET";
+$getfield = '?q=%40twitterapi';
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest(); 
 ?>
