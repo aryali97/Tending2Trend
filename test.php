a0805b30ab39e13a1918851dbe06cddc15258b72
aryali97 <aryali97@gmail.com>
2015-08-08 15:31:23 -0400

    Test1

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+require_once('TwitterAPIExchange.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "895890626-EBKN7DgWCMNoWQCUDwUPTL4ZIQDtztcs4P7S95ff",
+    'oauth_access_token_secret' => "DhCpuL5e6eAADunFshqMs45hSe3icxFLSoRYqqMi8BFGg",
+    'consumer_key' => "QTe03opZ9XcXsLwj55BRXb4qx",
+    'consumer_secret' => "wRWOKG7ofG4604cgg0uTVOSdmWuVu4lL8AMCEdq3XNI1TuUn3U"
+);
+?>
