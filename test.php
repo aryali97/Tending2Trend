071a526f500c4c13271dcec4d3d68be5c27164fc
aryali97 <aryali97@gmail.com>
2015-08-08 15:53:54 -0400

    test4

--- test.php
+++ test.php
@@ -1,6 +1,4 @@
 <?php
-require_once('TwitterAPIExchange.php');
-
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
     'oauth_access_token' => "895890626-EBKN7DgWCMNoWQCUDwUPTL4ZIQDtztcs4P7S95ff",
@@ -8,11 +6,17 @@ $settings = array(
     'consumer_key' => "QTe03opZ9XcXsLwj55BRXb4qx",
     'consumer_secret' => "wRWOKG7ofG4604cgg0uTVOSdmWuVu4lL8AMCEdq3XNI1TuUn3U"
 );
-$url = 'https://api.twitter.com/1.1/search/tweets.json';
+
+require_once('TwitterAPIExchange.php');
+ 
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+ 
 $requestMethod = "GET";
-$getfield = '?q=%40twitterapi';
+ 
+$getfield = '?screen_name=iagdotme&count=20';
+ 
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
              ->buildOauth($url, $requestMethod)
-             ->performRequest(); 
+             ->performRequest();
 ?>
