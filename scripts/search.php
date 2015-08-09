296f8e54f71cdca06c7cf3ae36ffebb060024cd8
aryali97 <aryali97@gmail.com>
2015-08-09 16:48:27 -0400

    Displays markers by date

--- scripts/search.php
+++ scripts/search.php
@@ -1,6 +1,8 @@
 <?php
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $search = $_REQUEST["search"];
+$lat = $_REQUEST["lat"];
+$lon = $_REQUEST["lon"];
 $settings = array(
     'oauth_access_token' => "895890626-EBKN7DgWCMNoWQCUDwUPTL4ZIQDtztcs4P7S95ff",
     'oauth_access_token_secret' => "DhCpuL5e6eAADunFshqMs45hSe3icxFLSoRYqqMi8BFGg",
@@ -15,12 +17,11 @@ $url = "https://api.twitter.com/1.1/search/tweets.json";
 $requestMethod = "GET";
  
 $getfield = '?q=#' . $search;
-$langlong = '38.825,-77.611';
-$radius = ',10000mi';
+$langlong = $lat . "," . $lon;
+$radius = ',1000mi';
 $geocode = 'geocode=' . $langlong . $radius;
 $count = 'count=100';
-$getfield .= '&' . $geocode . '&' . $count;
-
+$getfield .= '&' . $geocode . '&' . $until . '&' . $count;
 $twitter = new TwitterAPIExchange($settings);
 
 $result = $twitter->setGetfield($getfield)
